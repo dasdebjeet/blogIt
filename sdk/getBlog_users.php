74eed3886d242a32ce6cf10ba02d3e9766d075c1
Debjeet Das <debjeet194@gmail.com>
2022-08-03 23:09:08 +0530

    Modified -- Updated dashboard user management, Backend intregrated (Still working on search and pagination

--- sdk/getBlog_users.php
+++ sdk/getBlog_users.php
@@ -3,44 +3,22 @@
 
     class user extends database{
         public function get_userId(){
-            $stmt = $this->connect()->prepare("SELECT users.user_id FROM `users` ORDER BY users.user_id DESC");
-            // $stmt = $this->connect()->prepare("SELECT users.user_id, users.user_img, users.user_name, users.role, users.last_seen, users.status, COUNT(blog_id) as pubBlog_count FROM `users` LEFT JOIN `blog` ON users.user_id = blog.author_id WHERE blog.blog_status = 'publish' GROUP BY author_id");
+            $stmt = $this->connect()->prepare("SELECT users.user_id, users.user_img, users.user_name, users.role, users.last_seen, users.status FROM `users` ORDER BY users.role='admin' DESC, users.status='active' DESC");
             $stmt->execute();
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            // $results = json_decode($result);
-
-            if($result){                
-                for($i=0; $i<sizeof($result); $i++){
-                    echo $result[$i]->user_id.' - '."\t\t\t\t";
-                    // echo $results[$i];
-                }
-
-                // return json_encode($result);
-                return $result;
+            $arr = $result;
+            if($result){                         
+                for($i=0; $i<sizeof($result); $i++){                
+                    $user_id = $result[$i]["user_id"];                    
+                    $stmt = $this->connect()->prepare("SELECT COUNT(blog.blog_id) as pubBlog_count FROM `blog` LEFT JOIN `users` ON blog.author_id=users.user_id WHERE blog.author_id=? AND blog.blog_status='publish'");
+                    $stmt->execute([$user_id]);
+                    $result2 = $stmt->fetchAll(PDO::FETCH_ASSOC);       
+                    $arr[$i]['pubBlog_count'] = $result2[0]['pubBlog_count'];
+                }              
+                return $arr;
             }else{
                 return FALSE;
             }
         }
-
-
-        public function get_userData($user_id){
-            $stmt = $this->connect()->prepare("SELECT users.user_id, users.user_img, users.user_name, users.role, users.last_seen, users.status FROM `users` WHERE users.user_id=? ORDER BY users.role='admin' DESC, users.status='active' DESC;");
-            $stmt->execute([$user_id]);
-            $result1 = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-            $stmt = $this->connect()->prepare("SELECT COUNT(blog.blog_id) as pubBlog_count FROM `blog` WHERE blog.author_id=? AND blog.blog_status = 'publish'");
-            $stmt->execute([$user_id]);
-            $result2 = $stmt->fetchAll(PDO::FETCH_ASSOC);          
-
-            if($result1){
-                $data = array($result1, $result2);
-                return $data;
-            }else{
-                return FALSE;
-            }
-        }
-
-
     }
 ?>
--- sdk/setBlog_users.php
+++ sdk/setBlog_users.php
@@ -1,34 +1,17 @@
 <?php
     require_once "getblog_users.php";
 
-    if(isset($_POST["fetch_userId"]) == "blog_userId"){
+    if(isset($_REQUEST["fetch_userId"]) == "blog_userId"){
         $obj = new user;
 
         $result = $obj->get_userId();
-        // $results = json_decode($result);
+        $result = json_encode($result);
         if($result){
-            // for($i=0; $i<sizeof($results); $i++){
-            //     // echo $results[$i]->user_id.' - '.$results[$i]->user_name."\t\t\t\t";
-            //     echo $results[$i];
-            // }
-            // header('Content-Type: application/json');
-            print_r($result);
-        }else{
-            echo "couldn't fetch users!";
-        }
-    }
-
-
-    if(isset($_POST["fetch_userData"]) == "blog_userData"){
-
-        $obj = new user;
-
-        $result = $obj->get_userData($_POST["userId"]);
-        if($result){
-            $result = json_encode($result);
+            header('Content-Type: application/json');
             echo $result;
         }else{
             echo "couldn't fetch users!";
         }
     }
+
 ?>
