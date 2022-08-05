684e9b5c139e3c492102b45afcb42ccc8e697e16
Debjeet Das <debjeet194@gmail.com>
2022-08-05 14:48:02 +0530

    Modified -- Updated dashboard user management, Backend intregrated (Still working on pagination and table search)

--- js/dashboard_user_management.js
+++ js/dashboard_user_management.js
@@ -13,37 +13,82 @@ $(document).ready(() => {
             var page_lim = response['lim']
             var total_users = response['total_users']
             var total_pages = response['total_page']
-            var total_pages = 10
+            var total_pages = 2
 
+
+            var prev_page = 0
+            var cur_page = 1
+            var next_page = 2
 
             var page_arr = []
             if (total_pages > 4) {
-                page_arr.push('<div class="dashboard_userMgnt_pagination userMgnt_pagination_rnd" data_userTpage_count="prev"><i class="fal fa-angle-left"></i></div>')
+                page_arr.push('<div class="dashboard_userMgnt_pagination_btn userMgnt_pagination_rnd" data_userT_page_nav="prev" data_userTpage_count="prev"><i class="fal fa-angle-left"></i></div>')
                 for (var count = 1; count <= 3; count++) {
-                    if (count == 1) page_arr.push('<div class="dashboard_userMgnt_pagination userMgnt_pagination_active" data_userTpage_count="' + count + '">' + count + '</div>')
-                    else page_arr.push('<div class="dashboard_userMgnt_pagination" data_userTpage_count="' + count + '">' + count + '</div>')
+                    if (count == 1) page_arr.push('<div class="dashboard_userMgnt_pagination_count userMgnt_pagination_active" data_userTpage_count="' + count + '">' + count + '</div>')
+                    else page_arr.push('<div class="dashboard_userMgnt_pagination_count" data_userTpage_count="' + count + '">' + count + '</div>')
                 }
                 page_arr.push("...")
-                page_arr.push('<div class="dashboard_userMgnt_pagination" data_userTpage_count="' + total_pages + '">' + total_pages + '</div>')
-                page_arr.push('<div class="dashboard_userMgnt_pagination userMgnt_pagination_rnd" data_userTpage_count="next"><i class="fal fa-angle-right"></i></div>')
+                page_arr.push('<div class="dashboard_userMgnt_pagination_count" data_userTpage_count="' + total_pages + '">' + total_pages + '</div>')
+                page_arr.push('<div class="dashboard_userMgnt_pagination_btn userMgnt_pagination_rnd" data_userT_page_nav="next" data_userTpage_count="next"><i class="fal fa-angle-right"></i></div>')
             } else {
-                page_arr.push('<div class="dashboard_userMgnt_pagination userMgnt_pagination_rnd" data_userTpage_count="prev"><i class="fal fa-angle-left"></i></div>')
+                page_arr.push('<div class="dashboard_userMgnt_pagination_btn userMgnt_pagination_rnd" data_userT_page_nav="prev" data_userTpage_count="prev"><i class="fal fa-angle-left"></i></div>')
                 for (var count = 1; count <= total_pages; count++) {
-                    if (count == 1) page_arr.push('<div class="dashboard_userMgnt_pagination userMgnt_pagination_active" data_userTpage_count="' + count + '">' + count + '</div>')
-                    else page_arr.push('<div class="dashboard_userMgnt_pagination" data_userTpage_count="' + count + '">' + count + '</div>')
+                    if (count == 1) page_arr.push('<div class="dashboard_userMgnt_pagination_count userMgnt_pagination_active" data_userTpage_count="' + count + '">' + count + '</div>')
+                    else page_arr.push('<div class="dashboard_userMgnt_pagination_count" data_userTpage_count="' + count + '">' + count + '</div>')
                 }
-                page_arr.push('<div class="dashboard_userMgnt_pagination userMgnt_pagination_rnd" data_userTpage_count="next"><i class="fal fa-angle-right"></i></div>')
+                page_arr.push('<div class="dashboard_userMgnt_pagination_btn userMgnt_pagination_rnd" data_userT_page_nav="next" data_userTpage_count="next"><i class="fal fa-angle-right"></i></div>')
             }
 
 
             // console.log(page_arr)
             $(".dashboard_userMgnt_pagination_con").html(page_arr)
 
-            var pagination = document.querySelectorAll('.dashboard_userMgnt_pagination')
-            for (const pag_nav of pagination) {
+            var pagination_links = document.querySelectorAll('.dashboard_userMgnt_pagination_count')
+            for (const pag_link of pagination_links) {
+                pag_link.addEventListener('click', (e) => {
+                    console.log($(pag_link).attr('data_userTpage_count'))
+
+                    cur_page = $(pag_link).attr('data_userTpage_count')
+                    $(pagination_links).removeClass("userMgnt_pagination_active")
+                    $(pag_link).addClass("userMgnt_pagination_active")
+                })
+            }
+
+
+
+
+
+
+            var pagination_btn = document.querySelectorAll('.dashboard_userMgnt_pagination_btn')
+            for (const pag_nav of pagination_btn) {
                 pag_nav.addEventListener('click', (e) => {
-                    console.log($(pag_nav).attr('data_userTpage_count'))
-                    // $(pagination).removeClass("userMgnt_pagination_active")
+                    // console.log($(pag_nav).attr('data_userTpage_count'))
+                    var nav_direc = $(pag_nav).attr('data_userT_page_nav')
+                    var nav_count = $(pag_nav).attr('data_userTpage_count')
+
+                    // if (prev_page > -1 && next_page < total_pages) {
+                    if (nav_direc == "prev") {
+                        prev_page -= 1
+                        next_page -= 1
+                        cur_page -= 1
+                        $(pag_nav).attr('data_userTpage_count', prev_page)
+                        $(pagination_links).removeClass("userMgnt_pagination_active")
+                        $("[data_userTpage_count=" + cur_page + "]").addClass("userMgnt_pagination_active")
+
+                    } else if (nav_direc == "next") {
+                        prev_page += 1
+                        next_page += 1
+                        cur_page += 1
+                        $(pag_nav).attr('data_userTpage_count', next_page)
+                        $(pagination_links).removeClass("userMgnt_pagination_active")
+                        $("[data_userTpage_count=" + cur_page + "]").addClass("userMgnt_pagination_active")
+
+                    }
+                    console.log(prev_page, cur_page, next_page, page_arr[3])
+                    // }
+
+
+                    // $(pagination_btn).removeClass("userMgnt_pagination_active")
                     // $(pag_nav).addClass("userMgnt_pagination_active")
                 })
             }
